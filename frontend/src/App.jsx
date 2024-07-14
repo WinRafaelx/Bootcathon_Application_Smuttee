183c89a691ba83109c98d042b51b2ebfaa5a28d1
Sarun Khumthai <srnkhumthai@gmail.com>
2024-07-14 23:11:32 +0700

    feat: :sparkles: Authentication
    login + signup with api (change endpoint)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ScheduleManagement from "./pages/Admin/ScheduleManagement";
+import Signup from "./pages/Admin/Auth/Signup";
+import Login from "./pages/Admin/Auth/Login";
 
 function App() {
   return (
@@ -8,7 +10,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/admin" element={<ScheduleManagement />} />
+          <Route path="/admin"  >
+            <Route index element={<ScheduleManagement />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login/>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/pages/Admin/Auth/Signup.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get("email");
+    const password = data.get("password");
+    const jsonData = {
+      email,
+      password,
+    };
+
+    alert("Registration Successful");
+    console.log(jsonData);
+
+    // try {
+    //   const response = await axios.post(
+    //     "http://localhost:3333/admin/auth/register", // Correct endpoint
+    //     jsonData,
+    //   );
+    //   if (response.data.status === "ok") {
+    //     alert("Registration Successful");
+    //     window.location = "/admin/login";
+    //   } else {
+    //     setError(response.data.message);
+    //   }
+    // } catch (error) {
+    //   console.error("Error:", error);
+    //   setError("An error occurred during registration");
+    // }
+  };
+  return (
+    <div className="h-screen">
+      <div className="flex min-h-full flex-col justify-center content-center px-6 py-12 ">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign up
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline "
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
