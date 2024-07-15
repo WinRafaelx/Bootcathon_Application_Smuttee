2a840b42d2b3a5858d0eaf4a2d8c90de08ff0db6
Trollface8D <82329632+Trollface8D@users.noreply.github.com>
2024-07-15 15:49:53 +0700

    feat: :sparkles: Create service page for selecting service, And ServiceCard and Header component

---
+++ frontend/src/pages/Service/service.jsx
@@ -0,0 +1,36 @@
+//import ServiceCard from '../../components/service/ServiceCard.jsx';
+
+const services = [
+  {
+    image: 'https://fastly.picsum.photos/id/915/200/300.jpg?hmac=dzVwmjYlIh3MdKz2l08oFpp1y3XxMyu_1vjGp5Dycd0',
+    title: 'เปลี่ยนถ่ายน้ำมันเครื่อง',
+  },
+  {
+    image: 'https://fastly.picsum.photos/id/915/200/300.jpg?hmac=dzVwmjYlIh3MdKz2l08oFpp1y3XxMyu_1vjGp5Dycd0',
+    title: 'ตรวจเช็คสภาพรถยนต์',
+  },
+  {
+    image: 'https://fastly.picsum.photos/id/915/200/300.jpg?hmac=dzVwmjYlIh3MdKz2l08oFpp1y3XxMyu_1vjGp5Dycd0',
+    title: 'อื่นๆ',
+  },
+];
+
+
+export default function Service() {
+  return (
+    <div>service</div>
+    // <div className="bg-gray-100 min-h-screen p-4">
+    //   <header className="text-center mb-6">
+    //     <h1 className="text-3xl font-bold">Mobil Support</h1>
+    //   </header>
+    //   <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+    //     {services.map((service, index) => (
+    //       <ServiceCard image={service.image} title={service.title} />
+    //     ))}
+    //   </div>
+    //   <div className="text-center mt-6">
+    //     <button className="bg-blue-500 text-white py-2 px-4 rounded">ถัดไป</button>
+    //   </div>
+    // </div>
+  );
+}
