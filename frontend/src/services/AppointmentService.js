9462dd19f0906cd3e3d6ede464f900ebb182a992
Sarun Khumthai <srnkhumthai@gmail.com>
2024-07-17 07:46:13 +0700

    Update AppointmentService.js

--- frontend/src/services/AppointmentService.js
+++ frontend/src/services/AppointmentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/appointments';
+const API_URL = 'http://localhost:8000/api/workshop_admin/getReservations';
 
 const getAppointment = () => {
   return axios.get(API_URL);
